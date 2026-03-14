9d04b6ddcea7768c1a37e552d5849cac00856913
Charles Coverdale <charlesfcoverdale@gmail.com>
2026-03-14 19:33:09 +0000

    Harden HTTP resilience and fix stale documentation
    - Add req_retry(), req_user_agent(), req_throttle() to resolve_govuk_url() and download_cached()
    - Fix data.R row count (29 -> 41) to match actual tax_heads data
    - Update hmrc-package.R to list all 12 exported functions with correct date ranges
    - Fix tautological test in test-get_tax_receipts.R

--- R/utils-govuk-api.R
+++ R/utils-govuk-api.R
@@ -19,7 +19,10 @@ resolve_govuk_url <- function(slug, filename_pattern) {
 
   resp <- tryCatch(
     httr2::request(api_url) |>
+      httr2::req_user_agent("hmrc R package (https://github.com/charlescoverdale/hmrc)") |>
+      httr2::req_throttle(rate = 5 / 10) |>
       httr2::req_timeout(30) |>
+      httr2::req_retry(max_tries = 3) |>
       httr2::req_error(is_error = function(r) FALSE) |>
       httr2::req_perform(),
     error = function(e) {
@@ -84,7 +87,10 @@ download_cached <- function(url, cache = TRUE) {
 
   tryCatch(
     httr2::request(url) |>
+      httr2::req_user_agent("hmrc R package (https://github.com/charlescoverdale/hmrc)") |>
+      httr2::req_throttle(rate = 5 / 10) |>
       httr2::req_timeout(120) |>
+      httr2::req_retry(max_tries = 3) |>
       httr2::req_perform(path = cache_file),
     error = function(e) {
       if (file.exists(cache_file)) unlink(cache_file)
